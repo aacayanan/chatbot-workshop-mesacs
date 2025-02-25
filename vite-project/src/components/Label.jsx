bca99ac39da9c60a8eaf74f2a156429a7e00afba
Osmium <aaroncayanan02@gmail.com>
2025-02-24 22:42:47 -0800

    Work on label and info button, started html for the text box area.

--- vite-project/src/components/Label.jsx
+++ vite-project/src/components/Label.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../stylesheets/Label.css';
+import SendButton from '../assets/sendbutton.png';
 
 Label.propTypes = {
 
 };
 
 function Label(props) {
+    const handleSendClick = () => {
+        const textBox = document.querySelector('.text-box');
+        const message = textBox.value;
+        if (message !== '') {
+            console.log(message);
+            textBox.value = '';
+        }
+    };
+
     return (
         <div>
-            <div className='label-text'>
-                AI Chat Bot
+            <div className='label-container'>
+                <div className='label-text'>
+                    AI Chat Bot
+                </div>
+                <div className='info-button-container'>
+                    <a className='info-button'>i</a>
+                </div>
+            </div>
+            <div>
+                <div className='text-box-container'>
+                    <input type='text' className='text-box' placeholder='Type a message...' />
+                </div>
+                <div className='send-button-container'>
+                    <img src={SendButton} alt='Send' className='send-button' onClick={handleSendClick}/>
+                </div>
             </div>
         </div>
     );
